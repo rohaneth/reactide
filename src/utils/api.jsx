3701e1665b8790849104187610320401206ea030
rohaneth <rohanmuttepwar55@gmail.com>
2025-04-20 13:14:27 +0530

    change

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: ' https://7e3b-2409-4081-1e-1c9c-7955-3e55-1d71-ed2.ngrok-free.app',
 });
 
 API.interceptors.request.use(config => {
